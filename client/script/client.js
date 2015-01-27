87c16577927fc437a4774125996da7d1fbbb2c6e
simli357 <simli357@student.liu.se>
2015-01-27 15:20:52 +0100

    added an error message function

--- client/script/client.js
+++ client/script/client.js
@@ -1,5 +1,3 @@
-$("#error-message-box").hide();
-
 displayView = function(currentView){	
 	$('body').append(document.getElementById(currentView).text);
 	$('body').css('background-size', '110% 130%')
@@ -17,18 +15,46 @@ window.onload = function(){
 	displayView(view);
 }
 
+expandBox = function(boxID, newElement, expand) {
+	var box = document.getElementById(boxID);
+	var newHeight = 278 + expand*document.getElementById(newElement).offsetHeight;
+
+	if (expand == true) {
+		box.setAttribute("style","height: " + newHeight + "px")	
+	} else {
+		box.setAttribute("style","height: 276px")
+	}
+}
 
 displayErrorMessage = function(message) {
+	//var div = document.getElementById("error-message-box");
+	var p = document.getElementById("error-message");
 
+	if (message == "nothing") {
+		expandBox("signUp","error-message", false);
+		p.innerHTML = message;
+		p.style.display = 'none'; // changed from div
+	} else {
+		if (p.innerHTML != message) {
+			p.innerHTML = message;		
+		}
+		p.style.display = 'block'; //  changed from div	
+		expandBox("signUp","error-message", true)
+	}	
 }
 
 signupClick = function() {
-	var invalidMessage = null;
+	var errorMessage = "nothing";
+	var password = document.getElementById("password-SU").value;
 
-	if (document.getElementById("password-SU").value != document.getElementById("repeatPassword-SU")) {
-		invalidMessage = "Not the same as password";
+	console.log(password);
+	if (password.length < 5) {
+		errorMessage = "The password must be at least 5 charactes long"
+	} else if (password != document.getElementById("repeatPassword-SU").value) {
+		errorMessage = "Not the same as password";
 	}
 
-	displayErrorMessage(invalidMessage)
+	displayErrorMessage(errorMessage);
 
 }
+
