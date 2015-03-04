ce5baa411d81312ba83c9d7f553ccbbb940c22a0
Mikael Lietha <mikli278@student.liu.se>
2015-03-04 20:46:34 +0100

    Implemented all the functionality in database_helper.py

--- database/database_helper.py
+++ database/database_helper.py
@@ -24,28 +24,46 @@ def sign_up(email, password, firstname, familyname, gender, city, country):
         cur = con.cursor()
         cur.execute('INSERT INTO users (email, password, firstname, familyname, gender, city, country) VALUES (?,?,?,?,?,?,?)', (email, password, firstname, familyname, gender, city, country))
 
+
 def sign_in(email, password):
     with con:
         cur = con.cursor()
-        psw_in_db = cur.execute('SELECT password FROM users WHERE email = ?', email)
-        if psw_in_db = password:
+        cur.execute('SELECT password FROM users WHERE email = ?', (email,))
+        data = cur.fetchone()
+        if data[0] == password:
             return True
         else:
             return False
 
 
 
+
 def change_password(email, new_password):
+    with con:
+        cur = con.cursor()
+        cur.execute('UPDATE users SET password = ? WHERE email = ?', (new_password,email))
 
 
 def get_user_data_by_email(email):
+    with con:
+        cur = con.cursor()
+        cur.execute('SELECT * FROM users WHERE email = ?',(email,))
+        data = cur.fetchone()
+        return data
 
 
 def get_user_message_by_email(email):
+    with con:
+        cur = con.cursor()
+        cur.execute('SELECT * FROM messages WHERE email_poster = ?',(email,))
+        data = cur.fetchall()
+        return data
     
 
 def post_message (poster_email, message, wall_email):
-
+    with con:
+        cur = con.cursor()
+        cur.execute('INSERT INTO messages(email_poster,email_wall,message) values(?,?,?)',(poster_email,wall_email,message))
 
 
 
@@ -58,5 +76,18 @@ if __name__ == '__main__':
 	app.debub = True
 	app.run()
         init_db()
-        sign_up('anders.lietha@gmail.com','asd', 'mikael','lietha','male', 'linkoping','sweden')
-        print("Hello")
+        sign_up('Sven@gmail.com','asd', 'Sven','Balle','male', 'linkoping','sweden')
+        sign_up('Bengt@hotmail.com','qwerty','Bengt','Ballong','MEJL','STHLM','Denmark')
+        change_password('Sven@gmail.com','Svennebanan')
+        print(get_user_data_by_email('Bengt@hotmail.com'))
+        post_message('Sven@gmail.com','Alla suger','Bengt@gmail.com')
+        post_message('Sven@gmail.com','Nagra suger','Bengt@gmail.com')
+        post_message('Sven@gmail.com','Ingen suger','Bengt@gmail.com')
+        post_message('Bengt@hotmail.com','Etta','Sven@gmail.com')
+        post_message('Bengt@hotmail.com','Tvaa','Sven@gmail.com')
+        post_message('Bengt@hotmail.com','Trea','Sven@gmail.com')
+        print(get_user_message_by_email('Bengt@hotmail.com'))
+        print(sign_in('Sven@gmail.com','asd'))
+        print(sign_in('Sven@gmail.com','Svennebanan'))
+
+
